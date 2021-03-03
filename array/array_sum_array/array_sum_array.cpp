7db40502a3f56b324f5e6d914f05d3192abf108d
cx <cx>
2021-03-03 09:18:08 +0800

    add

--- array/array_sum_array/array_sum_array.cpp
+++ array/array_sum_array/array_sum_array.cpp
@@ -1,4 +1,4 @@
-long sum_array(int* arr, int len)
+long array_sum_array(int* arr, int len)
 {
     if (arr == nullptr) return -1;
     if (len <= 0) return -1;
@@ -8,9 +8,9 @@ long sum_array(int* arr, int len)
     return result;
 }
 
-long sum_array2(int* arr, int left, int right)
+long array_sum_array2(int* arr, int left, int right)
 {
 	if (arr == nullptr) return 0;
 	if (left >= right) return 0;
-	return sum_array2(arr, left+1, right) + arr[left];
+	return array_sum_array2(arr, left+1, right) + arr[left];
 }
